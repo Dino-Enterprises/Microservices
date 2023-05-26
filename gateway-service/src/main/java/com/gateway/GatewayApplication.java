210171cd4de427366ae493d7c3a065f2be0e230e
EnzoLivelli2000 <58576353+EnzoLivelli2000@users.noreply.github.com>
2023-05-26 16:18:48 -0500

    Update GatewayApplication.java

--- gateway-service/src/main/java/com/gateway/GatewayApplication.java
+++ gateway-service/src/main/java/com/gateway/GatewayApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+/*Docoradores para iniciar el proyecto*/
 @EnableEurekaClient
 @SpringBootApplication
 public class GatewayApplication {
