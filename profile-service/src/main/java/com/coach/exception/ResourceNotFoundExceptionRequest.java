80c9e65d8eb57eb4ba8c0c9b7368afe7258924ea
Renato <u201511892@upc.edu.pe>
2023-05-26 17:11:16 -0500

    feat: notfound

--- profile-service/src/main/java/com/coach/exception/ResourceNotFoundExceptionRequest.java
+++ profile-service/src/main/java/com/coach/exception/ResourceNotFoundExceptionRequest.java
@@ -3,6 +3,7 @@ package com.coach.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+// NotFound
 @ResponseStatus(HttpStatus.NOT_FOUND)
 public class ResourceNotFoundExceptionRequest extends RuntimeException {
     public ResourceNotFoundExceptionRequest(String message) {
