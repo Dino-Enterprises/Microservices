2a039395020108940e9edde6d0daad78a4dd8b60
EnzoLivelli2000 <58576353+EnzoLivelli2000@users.noreply.github.com>
2023-05-26 16:31:44 -0500

    Update ResourceNotFoundExceptionRequest.java

--- payment-service/src/main/java/com/game_coin/exception/ResourceNotFoundExceptionRequest.java
+++ payment-service/src/main/java/com/game_coin/exception/ResourceNotFoundExceptionRequest.java
@@ -3,6 +3,7 @@ package com.game_coin.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+/*Esta clase fue creada cuando no se puede encontrar un recurso*/
 @ResponseStatus(HttpStatus.NOT_FOUND)
 public class ResourceNotFoundExceptionRequest extends RuntimeException {
     public ResourceNotFoundExceptionRequest(String message) {
