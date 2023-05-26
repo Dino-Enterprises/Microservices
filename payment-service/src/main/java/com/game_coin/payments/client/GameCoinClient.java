11f32cc393994df32a664b498d14cc2bb2760198
FernandoH <u201917968@upc.edu.pe>
2023-05-26 17:33:01 -0500

    Update GameCoinClient.java

--- payment-service/src/main/java/com/game_coin/payments/client/GameCoinClient.java
+++ payment-service/src/main/java/com/game_coin/payments/client/GameCoinClient.java
@@ -6,6 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+//GameCoin interface
 @FeignClient(name = "payment-service", path = "/game-coin-offers", fallback = GameCoinHystrixFallbackFactory.class)
 public interface GameCoinClient {
     @GetMapping("/{id}")
