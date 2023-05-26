f04f77ac8f3709936f6ebdfd259a2324c7b20338
Renato <u201511892@upc.edu.pe>
2023-05-26 17:07:44 -0500

    feat: bcrypt profile

--- profile-service/src/main/java/com/coach/config/SecurityConfig.java
+++ profile-service/src/main/java/com/coach/config/SecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+// BCrypt
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
