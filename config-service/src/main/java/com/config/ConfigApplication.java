f593ef4aebdab3d33ef5e5c3856abe3ec6610b4f
EnzoLivelli2000 <58576353+EnzoLivelli2000@users.noreply.github.com>
2023-05-26 16:11:16 -0500

    Update ConfigApplication.java

--- config-service/src/main/java/com/config/ConfigApplication.java
+++ config-service/src/main/java/com/config/ConfigApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
+/*Decoradores para poder iniciar el proyecto*/
 @EnableConfigServer
 @SpringBootApplication
 public class ConfigApplication {
