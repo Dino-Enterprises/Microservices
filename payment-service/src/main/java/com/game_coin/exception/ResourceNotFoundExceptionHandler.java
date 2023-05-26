5d964f47751d39115bd53b08dd7a745763408ec0
EnzoLivelli2000 <58576353+EnzoLivelli2000@users.noreply.github.com>
2023-05-26 16:27:34 -0500

    Update ResourceNotFoundExceptionHandler.java

--- payment-service/src/main/java/com/game_coin/exception/ResourceNotFoundExceptionHandler.java
+++ payment-service/src/main/java/com/game_coin/exception/ResourceNotFoundExceptionHandler.java
@@ -5,6 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+/*Esta clase fue creada cuando no se puede encontrar un recurso*/
 @ControllerAdvice
 public class ResourceNotFoundExceptionHandler {
     @ExceptionHandler(value = { ResourceNotFoundExceptionRequest.class })
