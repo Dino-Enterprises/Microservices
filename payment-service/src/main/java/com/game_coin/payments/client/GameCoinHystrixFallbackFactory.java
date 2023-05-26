61b93e3f3c8805586e6648cfbb71edeb89559318
FernandoH <u201917968@upc.edu.pe>
2023-05-26 17:34:37 -0500

    Update GameCoinHystrixFallbackFactory.java

--- payment-service/src/main/java/com/game_coin/payments/client/GameCoinHystrixFallbackFactory.java
+++ payment-service/src/main/java/com/game_coin/payments/client/GameCoinHystrixFallbackFactory.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class GameCoinHystrixFallbackFactory implements GameCoinClient {
-
+//valores predeterminados
     @Override
     public ResponseEntity<GameCoin> getById(Long id) {
         GameCoin gameCoin = new GameCoin();
